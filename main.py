6a42cd2703865c8b0e528597e0edc03a40db69cd
SPinzon12 <samuel.pinzon@uao.edu.co>
2023-08-28 16:49:02 -0500

    Fix .gitignore to include proper files/folders

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import data_transformation
 
 if __name__ == "__main__":
     db_operations.create_connection()
-    candidates_df = data_transformation.transform_data("./data/candidates.csv")
+    candidates_df = data_transformation.transform_data("./dataset/candidates.csv")
     print(candidates_df[["IsHired", "CodeChallengeScore", "TechnicalInterviewScore"]].sample(10))
     db_operations.create_table()
     db_operations.insert_data(candidates_df)
